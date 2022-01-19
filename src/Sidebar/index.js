49aa1e51f9e6e2fb6d150b6390833f55dc4274c7
Dmitriy Yegorov <yegorovd14@gmail.com>
2022-01-19 15:33:24 +0000

    front end v1.1

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
 
-function Sidebar() {
+// import {
+//   BrowserRouter as Router,
+// //   Switch,
+// //   Route,
+//   Link
+// } from "react-router-dom";
+
+function Sidebar( { id, topic } ) {
+    // console.log(newData)
+    // const [focus, setFocus] = useState(false);
+
+
+    // function focusLink() {
+    //     setFocus(!focus);
+    // }
+
+    // let week = newData.map(week => <Link key={week.id} to={`/week${week.id}`} className={!focus ? "link-week": "link-active"} onClick={focusLink}>`Week ${week.id} - ${week.topic}`</Link>);
+
     return(
         <div className="sidebar">
-            <h3>Week 1</h3>
-            <h3>Week 2</h3>
-            <h3>Week 3</h3>
-
+            
+            <p className="link-week">Week ${id} - ${topic}</p>
+           
+            
+            {/* 
+            {/* <Link to='/week1' className={!focus ? "link-week": "link-active"} onClick={focusLink}>Week 1 - Foundation</Link>
+            
+            <Link to='/week3' className="link-week">Week 3 - Front End</Link> */}
+            
+            
+           
         </div>
     )
 }
