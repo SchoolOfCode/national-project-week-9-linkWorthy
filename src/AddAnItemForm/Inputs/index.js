b9193f7f43c57cc3979f5e5c6f1f1ab1e7b1cf35
priya <pmistry1707@gmail.com>
2022-01-20 23:18:36 +0000

    form component css updated

--- src/AddAnItemForm/Inputs/index.js
+++ src/AddAnItemForm/Inputs/index.js
@@ -1,45 +1,65 @@
-import React, { useState } from 'react';
-import './inputs.css';
+import React, { useState } from "react";
+import "./inputs.css";
 
 function Inputs({ onSubmit }) {
-    
-    const [language, setLanguage] = useState('');
-    const [link, setLink] = useState('');
-    const [summary, setSummary] = useState('');
-    
-    
-    function handleLanguages(event) {
-        // event.preventDefault();
-        const topic = event.target.value;
-        setLanguage(topic);
-    }
-    function handleLink(event) {
-        // event.preventDefault();
-        const link = event.target.value;
-        setLink(link);
-    }
-    function handleSummary(event) {
-        // event.preventDefault();
-        const summary = event.target.value;
-        setSummary(summary);
-    }
+	const [language, setLanguage] = useState("");
+	const [link, setLink] = useState("");
+	const [summary, setSummary] = useState("");
 
-    
-    return(
-        <div className="form">
-            <label> Languages   
-            </label>
-                <input className="form-input" type='text' onChange={handleLanguages} value={language}/>
-            <label> Useful link  
-            </label>
-                <input className="form-input" type='text' onChange={handleLink} value={link}/>
-            <label> Summary  
-            </label>
-                <textarea className="form-input" type='text' onChange={handleSummary} placeholder='Your summary' value={summary} maxLength="75"/>
-            <br/>
-            <button onClick={() => onSubmit(language, link, summary, setLanguage, setLink, setSummary)}>Submit</button>
-        </div>  
-    )
+	function handleLanguages(event) {
+		// event.preventDefault();
+		const topic = event.target.value;
+		setLanguage(topic);
+	}
+	function handleLink(event) {
+		// event.preventDefault();
+		const link = event.target.value;
+		setLink(link);
+	}
+	function handleSummary(event) {
+		// event.preventDefault();
+		const summary = event.target.value;
+		setSummary(summary);
+	}
+
+	return (
+		<div className="form">
+			{/* <label> Summary</label> */}
+			<textarea
+				className="form-input fi-desc"
+				type="text"
+				onChange={handleSummary}
+				placeholder="Enter a short description of your bookmark"
+				value={summary}
+				maxLength="75"
+			/>
+			{/* <label> Useful link</label> */}
+			<input
+				className="form-input"
+				type="text"
+				placeholder="Enter a URL"
+				onChange={handleLink}
+				value={link}
+			/>
+			{/* <label> Languages</label> */}
+			<input
+				className="form-input"
+				type="text"
+				onChange={handleLanguages}
+				placeholder="Add a tag"
+				value={language}
+			/>
+			<br />
+			<button
+				className="submit-button"
+				onClick={() =>
+					onSubmit(language, link, summary, setLanguage, setLink, setSummary)
+				}
+			>
+				SUBMIT
+			</button>
+		</div>
+	);
 }
 
 export default Inputs;
