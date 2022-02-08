65380dc3b634cadbc6ea80abbfa2e953a2cbb1fd
priya <pmistry1707@gmail.com>
2022-02-08 23:00:00 +0000

    dark/lght theme var colour changes

--- src/DarkLightSwitch/index.js
+++ src/DarkLightSwitch/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./styles.css";
 
 function DarkLightSwitch({ onClick }) {
-  return (
-    <label className="switch">
-      <input type="checkbox" onClick={onClick} />
+	return (
+		<label className="switch">
+			<input type="checkbox" onClick={onClick} />
+			<span className="slider round"></span>
+		</label>
 
-      <span className="slider round"></span>
-    </label>
-
-    // <button onClick={onClick}>Switch2</button>
-  );
+		// <button onClick={onClick}>Switch2</button>
+	);
 }
 
 export default DarkLightSwitch;
